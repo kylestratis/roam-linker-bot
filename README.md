fadfbdcb5d08971d68886556a347a3bc466d48bd
kylestratis <kyle.stratis@gmail.com>
2021-03-05 21:29:48 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # roam-linker-bot
+A simple Discord bot that looks for blockrefs and converts them to Roam links
+
+## Usage
+* Set up a bot in the Discord admin panel
+* Add your bot token and Roam graph name to a .env file in the same directory as bot.py,
+then deploy.
---
+++ roam_linker_bot/bot.py
@@ -0,0 +1,32 @@
+"""
+This is a Discord bot that replies to any messages that have a Roam blockref with a URL to that blockref.
+"""
+
+# Standard library imports
+import os
+import re
+
+# Third-party imports
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+ROAM_WORKSPACE = os.getenv("ROAM_WORKSPACE")
+
+intents = discord.Intents(messages=True)
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    blockrefs = re.findall(r"\(\((\w+?)\)\)", message.content)
+    for blockref in blockrefs:
+        url = f"https://roamresearch.com#/app/{ROAM_WORKSPACE}/page/{blockref}"
+        await message.channel.send(url)
+
+
+client.run(TOKEN)
